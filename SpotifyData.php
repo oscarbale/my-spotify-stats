65e075f2eff3e5d0d6d12fd6bea8987b2e2d945c
Oscar Bale <o.bale@hotmail.com>
2025-09-06 18:10:58 +0100

    Updated `SpotifyData` to initialise empty arrays upon `__construct()`

--- SpotifyData.php
+++ SpotifyData.php
@@ -1,14 +1,31 @@
 <?php
 
+include_once 'SpotifySong.php';
+
 class SpotifyData
 {
     public array $raw_json_data;
+    public array $field_names = [
+        'artist' => 'master_metadata_album_artist_name',
+        'album' => 'master_metadata_album_album_name',
+        'song' => 'master_metadata_track_name',
+        'track_id' => 'spotify_track_uri',
+        'was_skipped' => 'skipped',
+        'completed' => 'reason_end', // This field can contain multiple values, will need filtering
+        'time_played' => 'ts'
+    ];
+    public array $artists;
+    public array $albums;
+    public array $songs;
 
     public function __construct()
     {
         ini_set('memory_limit', '-1'); // temporary measure to prevent errors during development
 
         $this->raw_json_data = SpotifyData::get_raw_json_data();
+        $this->artists = [];
+        $this->albums = [];
+        $this->songs = [];
     }
 
     /**
