9c507da5bb6fe2267d3e1d267d4bd0e9eec9b151
Oscar Bale <o.bale@hotmail.com>
2025-09-07 04:02:18 +0100

    Added `SpotifyAlbum` class

---
+++ SpotifyAlbum.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once ('SpotifyBaseStats.php');
+include_once ('SpotifyArtist.php');
+include_once ('SpotifySong.php');
+
+class SpotifyAlbum extends SpotifyBaseStats
+{
+    public string $album_name;
+    public SpotifyArtist $artist;
+    public array $songs;
+
+    public function __construct(string $album_name, SpotifyArtist $artist){
+        $this-> album_name = $album_name;
+        $this-> artist = $artist;
+        $this-> songs = array();
+
+        // Set all the incrementable variables to 0
+        $this->times_played = 0;
+        $this->times_played_completely = 0;
+        $this->times_skipped = 0;
+
+        // Set $this->play_history to an empty array
+        $this->play_history = [];
+    }
+
+    public function add_song(SpotifySong $song): void{
+        if (!in_array($song->name, array_keys($this->songs))){
+            $this->songs[$song->name] = $song;
+        }
+        else{
+            $this->songs[$song->name]->update_state($song);
+        }
+    }
+
+}
